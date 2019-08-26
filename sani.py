3824b006bb739da462e365efa2f7fc874c2d314b
sanirabiu <54536049+sanirabiu@users.noreply.github.com>
2019-08-26 04:55:14 -0700

    Add files via upload

---
+++ sani.py
@@ -0,0 +1,10 @@
+#sani Rabiu Daboh
+#25/08/2019
+#A program to print x is raised to the powerof y and print log base 2 of x
+import math
+x = int(input("enter a number: "))
+y = int(input("enter another number: "))
+power = x**y
+print(x, "raised to the power of ",y, "is ",power)
+log=(x)
+print("log base2 of", x ,"is:",math.log2(x))
